5b1503579fc5fcc1903b606eb251ac016d9f5b96
funsafemath <funsafemath@proton.me>
2025-07-29 07:29:16 +0200

    Return a ProcessControlError instead of c_int to be compatible with ? operator Add the BPF module

--- src/prctl.rs
+++ src/prctl.rs
@@ -1,11 +1,27 @@
 //! A module that provides functions to get/set no_new_privs attribute that is required to set up the filters without CAP_SYS_ADMIN
 
 use core::ffi::c_long;
+use std::{error::Error, fmt::Display};
 
 use libc::{PR_GET_NO_NEW_PRIVS, PR_SET_NO_NEW_PRIVS, c_int, prctl};
 
+use crate::error::format_unknown_error;
+
+#[derive(Debug)]
+pub struct ProcessControlError {
+    pub error_code: c_int,
+}
+
+impl Display for ProcessControlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "prctl error ({})", format_unknown_error(self.error_code))
+    }
+}
+
+impl Error for ProcessControlError {}
+
 /// Set the no_new_privs attribute of the calling thread
-pub fn set_no_new_privileges() -> Result<(), c_int> {
+pub fn set_no_new_privileges() -> Result<(), ProcessControlError> {
     match unsafe {
         prctl(
             PR_SET_NO_NEW_PRIVS,
@@ -17,13 +33,15 @@ pub fn set_no_new_privileges() -> Result<(), c_int> {
         )
     } {
         0 => Ok(()),
-        -1 => Err(crate::error::errno()),
+        -1 => Err(ProcessControlError {
+            error_code: crate::error::errno(),
+        }),
         _ => unreachable!(),
     }
 }
 
 /// Get the no_new_privs attribute of the calling thread
-pub fn get_no_new_privileges() -> Result<bool, c_int> {
+pub fn get_no_new_privileges() -> Result<bool, ProcessControlError> {
     match unsafe {
         prctl(
             PR_GET_NO_NEW_PRIVS,
@@ -35,7 +53,9 @@ pub fn get_no_new_privileges() -> Result<bool, c_int> {
     } {
         1 => Ok(true),
         0 => Ok(false),
-        -1 => Err(crate::error::errno()),
+        -1 => Err(ProcessControlError {
+            error_code: crate::error::errno(),
+        }),
         _ => unreachable!(),
     }
 }
